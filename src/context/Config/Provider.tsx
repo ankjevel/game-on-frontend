eb2cbc980bdc5a9cbe976538110fba8a8d09ac0e
Dennis Pettersson <mail@dennispettersson.se>
2019-09-12 07:25:07 +0200

    chore: styling

--- src/context/Config/Provider.tsx
+++ src/context/Config/Provider.tsx
@@ -1,23 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import req, { setHost } from '../../utils/req'
 import Context from './context'
-import CConfig from 'CConfig'
 
 export const ConfigProvider = props => {
   const [value, setValue] = useState()
 
   useEffect(() => {
     const initConfig = async () => {
-      const config = await req({
-        url: '/js/config.json',
-      })
+      const config = await req({ url: '/js/config.json' })
 
       setHost(config.api)
 
-      setValue((state: MaybeNull<CConfig>) => ({
-        ...state,
-        ...config,
-      }))
+      setValue(state => ({ ...state, ...config }))
     }
 
     initConfig()
